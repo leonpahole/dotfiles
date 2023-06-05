4be691aeebe3f84a7b452c38b10566aa8755be3a
Leon Pahole <leon.pahole@gmail.com>
2023-06-05 15:05:06 +0200

    Add alacritty config

--- link.sh
+++ link.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ln -s $SCRIPT_DIR/nvim/ ~/.config/nvim
 ln -s $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
+ln -s $SCRIPT_DIR/.alacritty.yml ~/.alacritty.yml
