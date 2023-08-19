b99d57108753453b3444d85734157c3f7b2b5683
Leon Pahole <leon.pahole@gmail.com>
2023-08-19 17:29:30 +0200

    Enable copilot for markdown

--- nvim/lua/leonpahole/core/keymaps.lua
+++ nvim/lua/leonpahole/core/keymaps.lua
@@ -55,6 +55,7 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>")
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string globally
 keymap.set("n", "<leader>fc", '"zyiw:Telescope live_grep default_text=<C-r>z<cr>') -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
